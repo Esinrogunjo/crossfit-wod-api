cf879b117bb635ca9be3ea28dab8a11ed75227ce
Esinrogunjo <daudakale@gmail.com>
2022-05-06 21:38:16 +0100

    added routes and conrollers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 // For testing purposes
 app.get("/", (req, res) => {
   res.send("<h2>It's Working!</h2>");
 });
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
